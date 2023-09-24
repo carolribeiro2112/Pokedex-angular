1970196fab8a9af3ce4070e0148d387bc0c71656
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-09-24 19:49:51 -0300

    Feat: Creating shared components file and components inside it

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { SearchComponent } from './search/search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokeHeaderComponent,
+    SearchComponent,
+    PokeListComponent
+  ],
+  exports: [
+    PokeHeaderComponent,
+    SearchComponent,
+    PokeListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
