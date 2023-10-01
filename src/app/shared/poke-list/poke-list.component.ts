f7b1d009d6b9ea77fb4ac0bf6a1d6dc656f64ddb
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-09-30 22:51:40 -0300

    Feat: Api get all pokemons and show then on the screen

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,11 +7,19 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent implements OnInit{
+
+  public getAllPokemons: any
+
   constructor(
     private pokeApiService: PokeApiService
   ) {}
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe(response => console.log(response));
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      response => {
+        this.getAllPokemons = response.results
+        console.log(this.getAllPokemons)
+      }
+    );
   }
 }
